3c70806b7a82e0a22226594952fe0dbb83f66416
Rob Jarvis <jarvisrob@users.noreply.github.com>
2017-03-18 21:45:35 +1100

    harm search simple constr magnitude implemented as diff between fj and fj.lower or fj.upper with no normalisation

--- harmony_search.R
+++ harmony_search.R
@@ -27,6 +27,7 @@ HarmonySearch <- function(f, ..., x.type, x.lower, x.upper, f.lower, f.upper, fw
   if (is.null(hm.init)) {
 
     # Generate random vectors of decision variables to create harmonic memory (HM)
+    print("Creating harmonic memory ...")
     x.hm <- matrix(rep(0, hms * n.x), nrow = hms, ncol = n.x)
     for (j in 1:n.x) {
       if (x.type[j] == "continuous") {
@@ -46,11 +47,14 @@ HarmonySearch <- function(f, ..., x.type, x.lower, x.upper, f.lower, f.upper, fw
     f.hm <- matrix(unlist(f.res), nrow = hms, ncol = n.f, byrow = TRUE)
     #print(f.hm)
 
+    print("... Done")
+
   } else {
-    print(n.x)
-    print(ncol(hm.init))
+    print("Using provided harmonic memory")
+    #print(n.x)
+    #print(ncol(hm.init))
     x.hm <- hm.init[, 1:n.x]
-    f.hm <- hm.init[, (n.x + 1) : ncol(hm.init)]
+    f.hm <- hm.init[, (n.x + 1):ncol(hm.init)]
   }
 
   if (!minimize) {
@@ -59,20 +63,26 @@ HarmonySearch <- function(f, ..., x.type, x.lower, x.upper, f.lower, f.upper, fw
 
   # Check constraints
   #flag.x.constr.violated <- matrix(rep(0, hms * n.x), nrow = hms, ncol = n.x)
-  flag.f.constr.violated <- matrix(rep(0, hms * n.f), nrow = hms, ncol = n.f)
+  f.cv <- matrix(rep(0, hms * n.f), nrow = hms, ncol = n.f)
   #for (j in 1:n.x) {
     #flag.x.constr.violated[, j] <- as.integer(x.hm[, j] < x.lower[j] | x.hm[, j] > x.upper[j])
   #}
   #print(flag.x.constr.violated)
   for (j in 1:n.f) {
-    flag.f.constr.violated[, j] <- as.integer(f.hm[, j] < f.lower[j] | f.hm[, j] > f.upper[j])
+    idx.cv.lower <- f.hm[, j] < f.lower[j]
+    f.cv[idx.cv.lower, j] <- f.lower[j] - f.hm[idx.cv.lower, j]
+    idx.cv.upper <- f.hm[, j] > f.upper[j]
+    f.cv[idx.cv.upper, j] <- f.hm[idx.cv.upper, j] - f.upper[j]
   }
   #flag.f.constr.violated <- as.integer(f.hm < f.lower | f.hm > f.upper)
   #print(flag.f.constr.violated)
-  total.constr.violated <- rowSums(flag.f.constr.violated)
+  f.cv.total <- rowSums(f.cv)
   #print(total.constr.violated)
+  #print(f.cv)
+  #print(f.cv.total)
 
   # Main iterative loop
+  print("Start main iterative loop ...")
   for (i in 1:itn.max) {
 
     # Generate new vector
@@ -123,43 +133,56 @@ HarmonySearch <- function(f, ..., x.type, x.lower, x.upper, f.lower, f.upper, fw
 
     # Check constraints
     #flag.x.new.constr.violated <- rep(0, n.x)
-    flag.f.new.constr.violated <- rep(0, n.f)
+    f.cv.new <- rep(0, n.f)
     #for (j in 1:n.x) {
       #flag.x.new.constr.violated[j] <- as.integer(x.new[j] < x.lower[j] | x.new[j] > x.upper[j])
     #}
-    for (j in 1:n.f) {
-      flag.f.new.constr.violated[j] <- as.integer(f.new[j] < f.lower[j] | f.new[j] > f.upper[j])
-    }
+    idx.cv.lower <- f.new < f.lower
+    f.cv.new[idx.cv.lower] <- f.lower[idx.cv.lower] - f.new[idx.cv.lower]
+    idx.cv.upper <- f.new > f.upper
+    f.cv.new[idx.cv.upper] <- f.new[idx.cv.upper] - f.upper[idx.cv.upper]
+
+    #for (j in 1:n.f) {
+      #flag.f.new.constr.violated[j] <- as.integer(f.new[j] < f.lower[j] | f.new[j] > f.upper[j])
+    #}
     #flag.f.new.constr.violated <- as.integer(f.new < f.lower | f.new > f.upper)
-    total.new.constr.violated <- sum(flag.f.new.constr.violated)
+    f.cv.new.total <- sum(f.cv.new)
     #print(flag.x.new.constr.violated)
     #print(flag.f.new.constr.violated)
     #print(total.new.constr.violated)
+    #print(f.cv.new)
+    #print(f.cv.new.total)
 
     # If f for new vector is better than worst f in HM, replace
-    idx.worst <- order(total.constr.violated, f.hm[, 1], decreasing = TRUE)[1]
-    total.constr.violated.worst <- total.constr.violated[idx.worst]
+    idx.worst <- order(f.cv.total, f.hm[, 1], decreasing = TRUE)[1]
+    f.cv.total.worst <- f.cv.total[idx.worst]
     f.worst <- f.hm[idx.worst, 1]
-    #print(cbind(x.hm, f.hm, total.constr.violated))
+    #print(cbind(x.hm, f.hm, f.cv.total))
     #print(idx.worst)
     #print(f.worst)
-    #print(total.constr.violated.worst)
+    #print(f.cv.total.worst)
     #f.worst <- max(f.hm)
     #idx.worst <- which.max(f.hm)
-    if ((total.new.constr.violated < total.constr.violated.worst) |
-        (total.new.constr.violated == total.constr.violated.worst) & f.new[1] < f.worst) {
-      x.hm[idx.worst, ] <- x.new
+    if ((f.cv.new.total < f.cv.total.worst) |
+        (f.cv.new.total == f.cv.total.worst) & f.new[1] < f.worst) {
+      print(paste0("Replacing | HM worst: cv = ", as.character(f.cv.total.worst),
+                   ", f1 = ", as.character(f.worst), " | New: cv = ",
+                   as.character(f.cv.new.total), ", f1 = ", as.character(f.new[1])))
+      x.hm[idx.worst,] <- x.new
       f.hm[idx.worst, ] <- f.new
       #flag.x.constr.violated[idx.worst, ] <- flag.x.new.constr.violated
-      flag.f.constr.violated[idx.worst, ] <- flag.f.new.constr.violated
-      total.constr.violated <- rowSums(flag.f.constr.violated)
+      f.cv[idx.worst, ] <- f.cv.new
+      f.cv.total <- rowSums(f.cv)
     }
 
   }
+  print("... Main iterative loop done")
 
   # Find the best f, its x vector, and constraint flags, i.e. the result of the optimisation
-  f1.best <- min(f.hm[, 1])
-  idx.best <- which.min(f.hm[, 1])
+  idx.best <- order(f.cv.total, f.hm[, 1])[1]
+  f1.best <- f.hm[idx.best, 1]
+  #f1.best <- min(f.hm[, 1])
+  #idx.best <- which.min(f.hm[, 1])
   if (!minimize) {
     f.hm[, 1] <- -f.hm[, 1]
     f1.best <- -f1.best
@@ -167,10 +190,11 @@ HarmonySearch <- function(f, ..., x.type, x.lower, x.upper, f.lower, f.upper, fw
   x.best <- x.hm[idx.best, ]
   f.best <- f.hm[idx.best, ]
   hm <- cbind(x.hm, f.hm)
-  constr.violated <- cbind(flag.f.constr.violated, total.constr.violated)
+  cv <- cbind(f.cv, f.cv.total)
 
   # Return best f, its x vector, and all of harmonic memory
-  harm.search.result <- list(f1.best = f1.best, x.best = x.best, f.best = f.best, idx.best = idx.best, hm = hm, constr.violated = constr.violated)
+  print("Harmony search complete")
+  harm.search.result <- list(f1.best = f1.best, x.best = x.best, f.best = f.best, idx.best = idx.best, hm = hm, cv = cv)
   harm.search.result
 }
 
